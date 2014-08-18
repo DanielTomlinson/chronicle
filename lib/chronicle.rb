fb9d8fe6951e9904dfad111abd57cf3db5aafd7e
Danielle Lancashire <dani@builds.terrible.systems>
2014-08-18 20:54:03 +0100

    Setup

--- lib/chronicle.rb
+++ lib/chronicle.rb
@@ -1,5 +1,16 @@
 require "chronicle/version"
+require "git"
 
 module Chronicle
   # Your code goes here...
+  class Generator
+    def initialize
+      @git = Git.open(working_dir, :log => Logger.new(STDOUT))
+    end
+    
+    def generate_between(first, last)
+      log = @git.log.between(first, last)
+      puts log
+    end
+  end
 end
